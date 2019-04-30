16c530f5066d7ec0cf03d0d80a6a336ff15c189c
Tom Woodward <twwoodward@me.com>
2019-04-30 14:26:21 -0400

    add local acf json saver

--- wp-snips.php
+++ wp-snips.php
@@ -247,3 +247,18 @@ function configure_tinymce($in) {
   }";
   return $in;
 }
+
+
+
+//ACF JSON SAVER
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+function my_acf_json_save_point( $path ) {
+    
+    // update path
+    $path = get_stylesheet_directory() . '/acf-json';
+    
+    // return
+    return $path;
+    
+}
