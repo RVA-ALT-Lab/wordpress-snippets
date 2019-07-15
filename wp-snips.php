c337730dd81ab2544ba2039327f60bc8cc3f87a2
Tom Woodward <twwoodward@me.com>
2019-07-15 09:10:11 -0400

    some organizational changes
    additions of things from custom rampages as well

--- wp-snips.php
+++ wp-snips.php
@@ -1,13 +1,5 @@
 <?php 
 
-//hide admin bar for non-admin
-add_action('after_setup_theme', 'remove_admin_bar');
- 
-function remove_admin_bar() {
-  if (!current_user_can('administrator') && !is_admin()) {
-    add_filter('show_admin_bar', '__return_false');
-  }
-}
 
 
 //menu style list pages for custom post types with hierarchy 
@@ -15,6 +7,21 @@ wp_list_pages ( array(
         	'post_type' => 'the-post-type'
         	)
 	)		
+
+
+//ALT TEXT FOR THUMBNAILS 
+$thumbnail_id = get_post_thumbnail_id( $post->ID );
+$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);   
+the_post_thumbnail( 'full', array( 'alt' => $alt ) ); 
+
+
+/*
+**
+MINIMAL DASHBOARD STUFF
+**
+*/
+
+
 
 //hide posts from other authors for author level users
 function posts_for_current_author($query) {
@@ -29,6 +36,9 @@ function posts_for_current_author($query) {
     return $query;
 }
 add_filter('pre_get_posts', 'posts_for_current_author');
+
+
+
 
 //skip directly to posts instead of dashboard on login
 //redirects from dashboard to edit post list 
@@ -51,6 +61,8 @@ if (current_user_can('level_10')) {
 add_action('admin_menu', 'remove_the_dashboard');
 
 
+
+
 //remove AIM and Jabber from profile page
 function modify_user_contact_methods( $user_contact ) { 
     // Remove user contact methods
@@ -60,9 +72,13 @@ function modify_user_contact_methods( $user_contact ) {
 }
 add_filter( 'user_contactmethods', 'modify_user_contact_methods' );
 
+
+
 //remove elements from the admin bar
 show_admin_bar( true ); //show the admin bar to everyone
  
+
+
 //remove a bunch of standard elements
 function my_admin_bar_render() {
     global $wp_admin_bar;
@@ -76,66 +92,33 @@ function my_admin_bar_render() {
 add_action( 'wp_before_admin_bar_render', 'my_admin_bar_render' );
 
 
-//CHANGE WP JSON RETURNS TO > 99
-
-add_filter( 'rest_post_collection_params', 'big_json_change_post_per_page', 10, 1 );
-
-function big_json_change_post_per_page( $params ) {
-    if ( isset( $params['per_page'] ) ) {
-        $params['per_page']['maximum'] = 200;
-    }
-
-    return $params;
+//hide admin bar for non-admin
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
+function remove_admin_bar() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    add_filter('show_admin_bar', '__return_false');
+  }
 }
 
 
-
-//ALT TEXT FOR THUMBNAILS 
-$thumbnail_id = get_post_thumbnail_id( $post->ID );
-$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);   
-the_post_thumbnail( 'full', array( 'alt' => $alt ) ); 
-
-
-
-
-//prevent addition of terms to custom taxonomy from https://wordpress.stackexchange.com/questions/112686/how-to-prevent-new-terms-being-added-to-a-custom-taxonomy
-add_action( 'pre_insert_term', function ( $term, $taxonomy )
-{
-    return ( 'topics' === $taxonomy )
-        ? new WP_Error( 'term_addition_blocked', __( 'You cannot add terms to this taxonomy' ) )
-        : $term;
-}, 0, 2 );
-
-
-
-//add to taxonomy o post type arguments to remove from menu contruction view 
+//add to taxonomy to post type arguments to remove from menu construction view 
  $args = array(
 'show_in_nav_menus' => false,
+
+
+/*------------------------------END DASHBOARD MINIMAL STUFF----------------------------------*/
+
 )
 
+/*
+**
+ACF STUFF
+**
+*/
 
- //add custom columns in editor list view based on https://code.tutsplus.com/articles/add-a-custom-column-in-posts-and-custom-post-types-admin-screen--wp-24934
-
- // ADD NEW COLUMN w header
-function make_event_columns_head($defaults) {
-    $defaults['make_event'] = 'Make Event';
-    return $defaults;
-}
- 
-// add the extra element
-function make_event_columns_content($column_name, $post_ID) {
-    if ($column_name == 'make_event') {
-       echo 'foo';
-    }
-}
-
-
-//for all posts
-add_filter('manage_posts_columns', 'make_event_columns_head'); 
-add_action('manage_posts_custom_column', 'make_event_columns_content', 10, 2);
-//just for custom post type named workshop
-add_filter('manage_workshop_posts_columns', 'make_event_columns_head'); 
-add_action('manage_workshop_posts_custom_column', 'make_event_columns_content', 10, 2);
+//SHOW HIDDEN CUSTOM FIELDS
+add_filter( 'is_protected_meta', '__return_false', 999 );
 
 
 //ACF allow us to see custom fields in editor view
@@ -152,86 +135,6 @@ function my_custom_sizes( $sizes ) {
 }
 
 
-//CUSTOM page template for custom post type from a plugin 
-function get_custom_post_type_template($single_template) {
-     global $post;
-
-     if ($post->post_type == '*********') {
-          $single_template = dirname( __FILE__ ) . '/*********.php';
-     }
-     return $single_template;
-}
-add_filter( 'single_template', 'get_custom_post_type_template' );   
-
-
-
-//LETS YOU CONTROL WHAT GETS STRIPPED IN CUT/PASTE TO MCE EDITOR 
-//fix cut paste drama from https://jonathannicol.com/blog/2015/02/19/clean-pasted-text-in-wordpress/
-add_filter('tiny_mce_before_init','configure_tinymce');
-
-/**
- * Customize TinyMCE's configuration
- *
- * @param   array
- * @return  array
- */
-function configure_tinymce($in) {
-  $in['paste_preprocess'] = "function(plugin, args){
-    // Strip all HTML tags except those we have whitelisted
-    var whitelist = 'p,b,strong,i,em,h2,h3,h4,h5,h6,ul,li,ol,a,href';
-    var stripped = jQuery('<div>' + args.content + '</div>');
-    var els = stripped.find('*').not(whitelist);
-    for (var i = els.length - 1; i >= 0; i--) {
-      var e = els[i];
-      jQuery(e).replaceWith(e.innerHTML);
-    }
-    // Strip all class and id attributes
-    stripped.find('*').removeAttr('id').removeAttr('class').removeAttr('style');
-    // Return the clean HTML
-    args.content = stripped.html();
-  }";
-  return $in;
-}
-
-//SHOW HIDDEN CUSTOM FIELDS
-add_filter( 'is_protected_meta', '__return_false', 999 );
-
-
-
-//REMOVE STUFF FROM CUT/PASTE VISUAL EDITOR
-//fix cut paste drama from https://jonathannicol.com/blog/2015/02/19/clean-pasted-text-in-wordpress/
-add_filter('tiny_mce_before_init','configure_tinymce');
-
-/**
- * Customize TinyMCE's configuration
- *
- * @param   array
- * @return  array
- */
-function configure_tinymce($in) {
-  $in['paste_preprocess'] = "function(plugin, args){
-    // Strip all HTML tags except those we have whitelisted
-    var whitelist = 'p,b,strong,i,em,h2,h3,h4,h5,h6,ul,li,ol,a,href';
-    var stripped = jQuery('<div>' + args.content + '</div>');
-    var els = stripped.find('*').not(whitelist);
-    for (var i = els.length - 1; i >= 0; i--) {
-      var e = els[i];
-      jQuery(e).replaceWith(e.innerHTML);
-    }
-    // Strip all class and id attributes
-    stripped.find('*').removeAttr('id').removeAttr('class').removeAttr('style');
-    // Return the clean HTML
-    args.content = stripped.html();
-  }";
-  return $in;
-}
-
-
-/*
-ACF LAND
-*/
-
-
 //ACF JSON SAVER
 add_filter('acf/settings/save_json', 'my_acf_json_save_point');
  
@@ -245,19 +148,6 @@ function my_acf_json_save_point( $path ) {
     
 }
 
-
-//add custom field (ACF in this case) to JSON endpoint for custom post type - faculty is the post type
-add_action( 'rest_api_init', 'add_faculty_type_to_json' );
-function add_faculty_type_to_json() {
-
-    register_rest_field(
-        'faculty',
-        'faculty_type',
-        array(
-            'get_callback'    => 'faculty_return_type',
-        )
-    );
-}
 
 // Return acf field staff_group
 function faculty_return_type( $object, $field_name, $request ) {
@@ -295,3 +185,235 @@ function sort_record_by_year( $value, $post_id, $field ) {
 }
 
 add_filter('acf/load_value/name=faculty_record', 'sort_record_by_year', 10, 3);//replace faculty record with your repeater/container field
+
+/*------------------------------END ACF----------------------------------*/
+
+
+/*
+**
+API STUFF
+**
+*/
+
+
+//CHANGE WP JSON RETURNS TO > 99
+
+add_filter( 'rest_post_collection_params', 'big_json_change_post_per_page', 10, 1 );
+
+function big_json_change_post_per_page( $params ) {
+    if ( isset( $params['per_page'] ) ) {
+        $params['per_page']['maximum'] = 200;
+    }
+
+    return $params;
+}
+
+
+
+
+
+//prevent addition of terms to custom taxonomy from https://wordpress.stackexchange.com/questions/112686/how-to-prevent-new-terms-being-added-to-a-custom-taxonomy
+add_action( 'pre_insert_term', function ( $term, $taxonomy )
+{
+    return ( 'topics' === $taxonomy )
+        ? new WP_Error( 'term_addition_blocked', __( 'You cannot add terms to this taxonomy' ) )
+        : $term;
+}, 0, 2 );
+
+
+
+//add custom field (ACF in this case) to JSON endpoint for custom post type - faculty is the post type
+add_action( 'rest_api_init', 'add_faculty_type_to_json' );
+function add_faculty_type_to_json() {
+
+    register_rest_field(
+        'faculty',
+        'faculty_type',
+        array(
+            'get_callback'    => 'faculty_return_type',
+        )
+    );
+}
+
+
+/*---------------------------------JSON MOD FOR ADDITIONAL SITE INFO----------------------------------*/
+
+function extraJsonData($response){
+    $blog_id = get_current_blog_id();
+    $blog_details = get_blog_details($blog_id);
+    $data = $response->data;
+    $data['created'] =$blog_details->registered;
+    $data['last_updated'] =$blog_details->last_updated;
+    $data['post_count'] =$blog_details->post_count;
+    $data['page_count'] = wp_count_posts('page','publish');
+    $response->set_data($data);
+    return $response;
+}
+
+add_filter('rest_index', 'extraJsonData');
+
+/*------------------------------END API----------------------------------*/
+
+
+
+
+ //add custom columns in editor list view based on https://code.tutsplus.com/articles/add-a-custom-column-in-posts-and-custom-post-types-admin-screen--wp-24934
+
+ // ADD NEW COLUMN w header
+function make_event_columns_head($defaults) {
+    $defaults['make_event'] = 'Make Event';
+    return $defaults;
+}
+ 
+// add the extra element
+function make_event_columns_content($column_name, $post_ID) {
+    if ($column_name == 'make_event') {
+       echo 'foo';
+    }
+}
+
+
+//for all posts
+add_filter('manage_posts_columns', 'make_event_columns_head'); 
+add_action('manage_posts_custom_column', 'make_event_columns_content', 10, 2);
+//just for custom post type named workshop
+add_filter('manage_workshop_posts_columns', 'make_event_columns_head'); 
+add_action('manage_workshop_posts_custom_column', 'make_event_columns_content', 10, 2);
+
+
+
+
+
+//CUSTOM page template for custom post type from a plugin 
+function get_custom_post_type_template($single_template) {
+     global $post;
+
+     if ($post->post_type == '*********') {
+          $single_template = dirname( __FILE__ ) . '/*********.php';
+     }
+     return $single_template;
+}
+add_filter( 'single_template', 'get_custom_post_type_template' );   
+
+
+
+//LETS YOU CONTROL WHAT GETS STRIPPED IN CUT/PASTE TO MCE EDITOR !!!REVISIT WITH TINYMCE OPTIONS!!!!
+//fix cut paste drama from https://jonathannicol.com/blog/2015/02/19/clean-pasted-text-in-wordpress/
+add_filter('tiny_mce_before_init','configure_tinymce');
+
+/**
+ * Customize TinyMCE's configuration
+ *
+ * @param   array
+ * @return  array
+ */
+function configure_tinymce($in) {
+  $in['paste_preprocess'] = "function(plugin, args){
+    // Strip all HTML tags except those we have whitelisted
+    var whitelist = 'p,b,strong,i,em,h2,h3,h4,h5,h6,ul,li,ol,a,href';
+    var stripped = jQuery('<div>' + args.content + '</div>');
+    var els = stripped.find('*').not(whitelist);
+    for (var i = els.length - 1; i >= 0; i--) {
+      var e = els[i];
+      jQuery(e).replaceWith(e.innerHTML);
+    }
+    // Strip all class and id attributes
+    stripped.find('*').removeAttr('id').removeAttr('class').removeAttr('style');
+    // Return the clean HTML
+    args.content = stripped.html();
+  }";
+  return $in;
+}
+
+
+
+
+//REMOVE STUFF FROM CUT/PASTE VISUAL EDITOR
+//fix cut paste drama from https://jonathannicol.com/blog/2015/02/19/clean-pasted-text-in-wordpress/
+add_filter('tiny_mce_before_init','configure_tinymce');
+
+/**
+ * Customize TinyMCE's configuration
+ *
+ * @param   array
+ * @return  array
+ */
+function configure_tinymce($in) {
+  $in['paste_preprocess'] = "function(plugin, args){
+    // Strip all HTML tags except those we have whitelisted
+    var whitelist = 'p,b,strong,i,em,h2,h3,h4,h5,h6,ul,li,ol,a,href';
+    var stripped = jQuery('<div>' + args.content + '</div>');
+    var els = stripped.find('*').not(whitelist);
+    for (var i = els.length - 1; i >= 0; i--) {
+      var e = els[i];
+      jQuery(e).replaceWith(e.innerHTML);
+    }
+    // Strip all class and id attributes
+    stripped.find('*').removeAttr('id').removeAttr('class').removeAttr('style');
+    // Return the clean HTML
+    args.content = stripped.html();
+  }";
+  return $in;
+}
+
+
+
+
+//make sure comments reflect display name from https://wordpress.stackexchange.com/questions/31694/comments-do-not-respect-display-name-setting-how-to-make-plugin-to-overcome-thi
+add_filter('get_comment_author', 'wpse31694_comment_author_display_name');
+function wpse31694_comment_author_display_name($author) {
+    global $comment;
+    if (!empty($comment->user_id)){
+        $user=get_userdata($comment->user_id);
+        $author=$user->display_name;
+    }
+
+    return $author;
+}
+
+
+//allow some additional file types for upload
+function my_custom_mime_types( $mimes ) {
+
+        // New allowed mime types.
+        $mimes['svg'] = 'image/svg+xml';
+        $mimes['svgz'] = 'image/svg+xml';
+        $mimes['studio3'] = 'application/octet-stream';
+
+        // Optional. Remove a mime type.
+        unset( $mimes['exe'] );
+
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'my_custom_mime_types' );
+
+
+
+/*------------------------------------ENABLE CSS FOR ADMINS-------------------------------------------------*/
+//from https://wordpress.org/plugins/multisite-custom-css/ just didn't want another plugin
+
+add_filter( 'map_meta_cap', 'multisite_custom_css_map_meta_cap', 20, 2 );
+function multisite_custom_css_map_meta_cap( $caps, $cap ) {
+    if ( 'edit_css' === $cap && is_multisite() ) {
+        $caps = array( 'edit_theme_options' );
+    }
+    return $caps;
+}
+
+
+
+/*------------------------------------H5P  ---------------------------------------------------*/
+// Make H5P embeds flexible
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+if ( is_plugin_active(  'h5p/h5p.php' ) ) {
+  //plugin is activated
+     add_action('wp_enqueue_scripts', 'h5pflex_widget_enqueue_script');
+}
+
+
+function h5pflex_widget_enqueue_script() {
+    $h5p_script = plugins_url( 'h5p/h5p-php-library/js/h5p-resizer.js', __DIR__);
+    wp_enqueue_script( 'h5p_flex', $h5p_script, true );
+
+    }
